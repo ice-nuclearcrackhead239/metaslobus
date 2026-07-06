6937486ec3dbd811b1585fb26cb467b76bd0e69a
Soumeh <soumeh@proton.me>
2026-07-06 15:23:15 +0200

    fixed custom keybinds being blocked with modifiers

--- src/main/java/com/serverboss/metaslobus/InclusiveKeyMapping.java
+++ src/main/java/com/serverboss/metaslobus/InclusiveKeyMapping.java
@@ -2,15 +2,16 @@ package com.serverboss.metaslobus;
 
 import com.mojang.blaze3d.platform.InputConstants;
 import net.minecraft.client.KeyMapping;
+import net.minecraftforge.client.settings.KeyConflictContext;
 
 public class InclusiveKeyMapping extends KeyMapping {
 
 	public InclusiveKeyMapping(String name, int key) {
-		super(name, GenericKeyConflictContext.INSTANCE, InputConstants.Type.KEYSYM, key, "key.categories.metaslobus");
+		super(name, KeyConflictContext.IN_GAME, InputConstants.Type.KEYSYM, key, "key.categories.metaslobus");
 	}
 
 	public InclusiveKeyMapping(String name, InputConstants.Type type, int key) {
-		super(name, GenericKeyConflictContext.INSTANCE, type, key, "key.categories.metaslobus");
+		super(name, KeyConflictContext.IN_GAME, type, key, "key.categories.metaslobus");
 	}
 
 }
